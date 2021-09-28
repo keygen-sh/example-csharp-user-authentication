9e85b4c504d9992bd22ab7ee83641e160a05b964
Zeke Gabrielse <ezekg@yahoo.com>
2021-09-28 09:13:58 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Keygen
+{
+  public RestClient Client = null;
+
+  public Keygen(string accountId)
+  {
+    Client = new RestClient($"https://api.keygen.sh/v1/accounts/{accountId}");
+  }
+
+  public Dictionary<string, object> AuthenticateAsUser(string email, string password)
+  {
+    var credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{email}:{password}"));
+    var request = new RestRequest("tokens", Method.POST);
+
+    request.AddHeader("Content-Type", "application/vnd.api+json");
+    request.AddHeader("Accept", "application/vnd.api+json");
+    request.AddHeader("Authorization", $"Basic {credentials}");
+
+    var response = Client.Execute<Dictionary<string, object>>(request);
+    if (response.Data.ContainsKey("errors"))
+    {
+      var errors = (List<Dictionary<string, object>>) response.Data["errors"];
+      if (errors != null)
+      {
+        Console.WriteLine("[ERROR] [AuthenticateAsUser] Status={0} Errors={1}", response.StatusCode, errors);
+
+        Environment.Exit(1);
+      }
+    }
+
+    return (Dictionary<string, object>) response.Data["data"];
+  }
+
+  public RestSharp.JsonArray ListLicensesForToken(string token)
+  {
+    var request = new RestRequest("licenses", Method.GET);
+
+    request.AddHeader("Authorization", $"Bearer {token}");
+    request.AddHeader("Content-Type", "application/vnd.api+json");
+    request.AddHeader("Accept", "application/vnd.api+json");
+
+    var response = Client.Execute<Dictionary<string, object>>(request);
+    if (response.Data.ContainsKey("errors"))
+    {
+      var errors = (RestSharp.JsonArray) response.Data["errors"];
+      if (errors != null)
+      {
+        Console.WriteLine("[ERROR] [ListLicensesForToken] Status={0} Errors={1}", response.StatusCode, errors);
+
+        Environment.Exit(1);
+      }
+    }
+
+    return (RestSharp.JsonArray) response.Data["data"];
+  }
+}
+
+class Program
+{
+  public static void Main (string[] args)
+  {
+    var keygen = new Keygen("demo");
+
+    // Authenticate and list the user's licenses
+    var userToken = keygen.AuthenticateAsUser("demo@example.com", "demo");
+    var attrs = (Dictionary<string, object>) userToken["attributes"];
+    var token = (string) attrs["token"];
+    var licenses = keygen.ListLicensesForToken(token);
+
+    // Print the overall results
+    Console.WriteLine("[INFO] [Main] Token={0} LicenseCount={1}", token, licenses.Count);
+  }
+}
---
+++ README.md
@@ -0,0 +1,29 @@
+# Example C# User Authentication
+
+This is an example of authenticating as a user and listing the user's licenses.
+The example is written in C# using the .NET framework.
+
+## Running the example
+
+First, install dependencies with [`dotnet`](https://docs.microsoft.com/en-us/dotnet/core/tools/dotnet):
+
+```
+dotnet restore
+```
+
+Then run the program:
+
+```
+dotnet run
+```
+
+You should see log output indicating the current token and the user's license count:
+
+```
+[INFO] [Main] Token=user-e1e1d317127822de147cd07060e8edv3 LicenseCount=10
+```
+
+## Questions?
+
+Reach out at [support@keygen.sh](mailto:support@keygen.sh) if you have any
+questions or concerns!
